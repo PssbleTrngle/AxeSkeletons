e954d53561bc65acdc815c925952b20845751373
Niklas Widmann <nik@somethingcatchy.net>
2024-08-16 13:43:15 +0200

    update gradle plugins

--- src/main/java/com/possible_triangle/axe_skeletons/mixin/ZombieMixin.java
+++ src/main/java/com/possible_triangle/axe_skeletons/mixin/ZombieMixin.java
@@ -11,8 +11,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.UUID;
-
 @Mixin(Zombie.class)
 public class ZombieMixin {
 
